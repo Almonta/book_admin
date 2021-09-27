203c4c94334e86f0a241aae8f70666c404d6f492
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-27 11:31:24 +0900

    コールバック

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,15 @@ class Book < ApplicationRecord
   has_many :authors, through: :book_authors
 
   validates :name, presence: true
-  validates :name, length:: { maximum: 25 }
+  validates :name, length: { maximum: 25 }
   validates :price, numericaliity: { greater_than_or_equal_to: 0 }
+
+  before_validation do
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "Lovely #{matched}"
+    end
+  end
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
 end
